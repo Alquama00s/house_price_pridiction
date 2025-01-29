d89057c99df3804319b18bc261d52cf3a232e318
Alquama <alquamasalim000@gmail.com>
2025-01-29 20:08:17 +0530

    init

---
+++ .ipynb_checkpoints/regression-checkpoint.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("dataset.csv")
+
+plt.figure(figsize=(10, 5))
+for a in df.columns[1:]:
+    print(a)
+    plt.plot(df["price"], df[a], marker="o", linestyle="-", color="b", label="Value")
+    plt.xlabel("price")
+    plt.ylabel(a)
+    plt.title("CSV Data Plot")
+    plt.legend()
+    plt.grid(True)
+    plt.xticks(rotation=45)  # Rotate x-axis labels for readability
+    plt.show()
+
+
+# plt.xticks(rotation=45)  # Rotate x-axis labels for readability
+# plt.show()
+
+
+# # plt.plot(df["price"], df["area"], marker="o", linestyle="-", color="b", label="Value")
+# # plt.plot(df["price"], df["bedrooms"], marker="o", linestyle="-", color="b", label="Value")
+
+
+# plt.xlabel("price")
+# plt.ylabel("area")
+# plt.title("CSV Data Plot")
+# plt.legend()
+# plt.grid(True)
+
+
+# plt.xticks(rotation=45)  # Rotate x-axis labels for readability
+# plt.show()
